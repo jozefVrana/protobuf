8147df4747479b4b2a4d85e41f84329a571df76d
Vrana, Jozef <jozef.vrana@ymail.com>
2019-01-03 08:01:03 +0100

    Carthage support

---
+++ objectivec/ProtocolBuffer_iOS/ProtocolBuffer_iOS.h
@@ -0,0 +1,18 @@
+//
+//  ProtocolBuffer_iOS.h
+//  ProtocolBuffer_iOS
+//
+//  Created by Vrana, Jozef on 03/01/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProtocolBuffer_iOS.
+FOUNDATION_EXPORT double ProtocolBuffer_iOSVersionNumber;
+
+//! Project version string for ProtocolBuffer_iOS.
+FOUNDATION_EXPORT const unsigned char ProtocolBuffer_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtocolBuffer_iOS/PublicHeader.h>
+
+
